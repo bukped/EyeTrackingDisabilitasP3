a610273f3ec6f76fe1023ed0ffea6cf3d4d2c1be
unknown <haryadiyusuf61@gmail.com>
2023-02-07 00:03:20 -0800

    revisi

--- Chapter 3/Opencv2.py
+++ Chapter 3/Opencv2.py
@@ -1,16 +1,14 @@
+#Install library OpenCV
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
- 
-img = cv2.imageread(‘namafile’)
- 
-np.copy() merupakan fungsi untuk mengcopy array dari image.
- 
-img_bgr = np.copy(img)
- 
-cv2.cvtColor(source_img, color_space) merupakan fungsi yang di gunakan untuk melakukan konversi color space. Seperti yang sudah kita dibahas sebelumnya defaul dari Opencv merupakan BGR.
- 
-cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB) aka melakukan konversi dari BGR menjadi RGB.
- 
-img_rgb = cv2.cvtCOLOR(img_bgr, cv2COLOR_BGR2RGB)
- 
+
+#Membaca gambar
+img = cv2.imread('kucing.png')
+#Menampilkan gambar
+cv2.imshow('kucing',img)
+#Menyimpan gambar
+cv2.imwrite('save_kucing.png',img)
+
+#Menunda windows terdestroy
+cv2.waitKey(0)
+#Mendestroy windows
+cv2.destroyAllWindows()
--- Chapter 4/mediapipe.py
+++ Chapter 4/mediapipe.py
@@ -1,104 +1,82 @@
-# Pengenalan Mediapipe
-
-
-# Import Libraries
 import cv2
-import time
 import mediapipe as mp
-# Grabbing the Holistic Model from Mediapipe and
-# Initializing the Model
-mp_holistic = mp.solutions.holistic
-holistic_model = mp_holistic.Holistic(
-	min_detection_confidence=0.5,
-	min_tracking_confidence=0.5
-)
-
-# Initializing the drawing utils for drawing the facial landmarks on image
 mp_drawing = mp.solutions.drawing_utils
-# Grabbing the Holistic Model from Mediapipe and
-# Initializing the Model
-mp_holistic = mp.solutions.holistic
-holistic_model = mp_holistic.Holistic(
-	min_detection_confidence=0.5,
-	min_tracking_confidence=0.5
-)
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
-# Initializing the drawing utils for drawing the facial landmarks on image
-mp_drawing = mp.solutions.drawing_utils
-# (0) in VideoCapture is used to connect to your computer's default camera
-capture = cv2.VideoCapture(0)
+# For static images:
+IMAGE_FILES = []
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-# Initializing current time and precious time for calculating the FPS
-previousTime = 0
-currentTime = 0
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
+      continue
+    image_height, image_width, _ = image.shape
+    annotated_image = image.copy()
+    for hand_landmarks in results.multi_hand_landmarks:
+      print('hand_landmarks:', hand_landmarks)
+      print(
+          f'Index finger tip coordinates: (',
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      )
+      mp_drawing.draw_landmarks(
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imwrite(
+        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    if not results.multi_hand_world_landmarks:
+      continue
+    for hand_world_landmarks in results.multi_hand_world_landmarks:
+      mp_drawing.plot_landmarks(
+        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
 
-while capture.isOpened():
-	# capture frame by frame
-	ret, frame = capture.read()
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
 
-	# resizing the frame for better view
-	frame = cv2.resize(frame, (800, 600))
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
 
-	# Converting the from BGR to RGB
-	image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-	# Making predictions using holistic model
-	# To improve performance, optionally mark the image as not writeable to
-	# pass by reference.
-	image.flags.writeable = False
-	results = holistic_model.process(image)
-	image.flags.writeable = True
-
-	# Converting back the RGB image to BGR
-	image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-	# Drawing the Facial Landmarks
-	mp_drawing.draw_landmarks(
-	image,
-	results.face_landmarks,
-	mp_holistic.FACEMESH_CONTOURS,
-	mp_drawing.DrawingSpec(
-		color=(255,0,255),
-		thickness=1,
-		circle_radius=1
-	),
-	mp_drawing.DrawingSpec(
-		color=(0,255,255),
-		thickness=1,
-		circle_radius=1
-	)
-	)
-
-	# Drawing Right hand Land Marks
-	mp_drawing.draw_landmarks(
-	image,
-	results.right_hand_landmarks,
-	mp_holistic.HAND_CONNECTIONS
-	)
-
-	# Drawing Left hand Land Marks
-	mp_drawing.draw_landmarks(
-	image,
-	results.left_hand_landmarks,
-	mp_holistic.HAND_CONNECTIONS
-	)
-	
-	# Calculating the FPS
-	currentTime = time.time()
-	fps = 1 / (currentTime-previousTime)
-	previousTime = currentTime
-	
-	# Displaying FPS on the image
-	cv2.putText(image, str(int(fps))+" FPS", (10, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
-
-	# Display the resulting image
-	cv2.imshow("Facial and Hand Landmarks", image)
-
-	# Enter key 'q' to break the loop
-	if cv2.waitKey(5) & 0xFF == ord('q'):
-		break
-
-# When all the process is done
-# Release the capture and destroy all windows
-capture.release()
-cv2.destroyAllWindows()
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
